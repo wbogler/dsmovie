bb50156061d6fe9178b0a957b224aa287f562668
Willian Bogler Da Silva <willian.silva@pti.org.br>
2022-05-09 12:10:44 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,35 @@
+import MovieCards from "components/MovieCards";
 import Pagination from "components/Navbar/Pagination";
 
-function Listing(){
-    return(
-        <Pagination />
+function Listing() {
+    return (
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCards />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCards />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCards />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCards />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCards />
+                    </div>
+                </div>
+
+            </div>
+
+
+        </>
+
     )
 }
 
